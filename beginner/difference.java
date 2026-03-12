c11b30cef78eb42c141bee76738847771b98602c
user <guilherme-siqueira2121@users.noreply.github.com>
2026-03-12 14:53:39 -0300

    difference exercise

---
+++ beginner/difference.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int difference = (a * b) - (c * d);
+
+        System.out.println("DIFERENCA = " + difference);
+    }
+}
+
+// -
